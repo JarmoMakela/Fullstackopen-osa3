fa341d0ba084087b019b42db3b8f3627a5fe781e
Jarmo <jarmo.makela@tietoevry.com>
2025-01-18 00:06:07 +0200

    tehtävät 3.17 ja 3.18

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const cors = require('cors')
-//const mongoose = require('mongoose')
 const Contact = require('./models/contact')
 
 const app = express()
@@ -10,27 +9,6 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 app.use(express.static('dist'))
-
-/*const url =
-  `mongodb+srv://fullstack:${password}@cluster0.br4pb.mongodb.net/contactApp?retryWrites=true&w=majority&appName=Cluster0`
-
-mongoose.set('strictQuery', false)
-mongoose.connect(url)
-
-const contactSchema = new mongoose.Schema({
-  name: String,
-  number: String
-})
-
-contactSchema.set('toJSON', {
-  transform: (document, returnedObject) => {
-    returnedObject.id = returnedObject._id.toString()
-    delete returnedObject._id
-    delete returnedObject._v
-  }
-})
-
-const Contact = mongoose.model('Contact', contactSchema)*/
 
 // Create new token
 morgan.token('body', (req) => req.method === 'POST' ? JSON.stringify(req.body) : '')
@@ -54,32 +32,6 @@ const errorHandler = (error, request, response, next) => {
   next(error)
 }
 
-let persons = [
-  {
-    id: "1",
-    name: "Arto Hellas",
-    number: "040-123456"
-  },
-  {
-    id: "2",
-    name: "Ada Lovelace",
-    number: "39-44-5323523"
-  },
-  {
-    id: "3",
-    name: "Dan Abramow",
-    number: "12-43-234345"
-  },
-  {
-    id: "4",
-    name: "Mary Poppendieck",
-    number: "39-23-6423122"
-  }
-]
-
-/*app.get('/api/persons', (request, response) => {
-  response.json(persons)
-})*/
 app.get('/api/persons', (request, response, next) => {
   Contact.find({}).then(contacts => {
     response.json(contacts)
@@ -87,16 +39,6 @@ app.get('/api/persons', (request, response, next) => {
   .catch(error => next(error))
 })
 
-/*app.get('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  const person = persons.find(p => p.id === id)
-
-  if (person) {
-    response.json(person)
-  } else {
-    response.status(404).end()
-  }
-})*/
 app.get('/api/persons/:id', (request, response, next) => {
   Contact.findById(request.params.id)
     .then(contact => {
@@ -109,25 +51,6 @@ app.get('/api/persons/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-/*app.post('/api/persons', (request, response) => {
-
-  // Check the validity of the input
-  if (!request.body.name || request.body.name.length === 0) {
-    return response.status(400).json({error: 'name missing'})
-  }
-  if (!request.body.number || request.body.number.length === 0) {
-    return response.status(400).json({error: 'number missing'})
-  }
-  if (persons.find(p => p.name === request.body.name)) {
-    return response.status(400).json({error: 'name must be unique'})
-  }
-
-  // Create random id for the new entry and add the entry into the "database"
-  const id = Math.floor(Math.random() * 999999999999).toString()
-  const person = {id: id, name: request.body.name, number: request.body.number}
-  persons = persons.concat(person)
-  response.json(person)
-})*/
 app.post('/api/persons', (request, response, next) => {
   const body = request.body
 
@@ -146,11 +69,24 @@ app.post('/api/persons', (request, response, next) => {
   .catch(error => next(error))
 })
 
-/*app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  persons = persons.filter(p => p.id !== id)
-  response.status(204).end()
-})*/
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+  const contact = {
+    name: body.name,
+    number: body.number
+  }
+
+  if (!body.name || !body.number) {
+    return response.status(400).json({error: 'content missing'})
+  }
+
+  Contact.findByIdAndUpdate(request.params.id, contact, { new: true })
+    .then(updatedContact => {
+      response.json(updatedContact)
+    })
+    .catch(error => next(error))
+})
+
 app.delete('/api/persons/:id', (request, response, next) => {
   Contact.findByIdAndDelete(request.params.id)
   .then(result => {
@@ -163,10 +99,14 @@ app.delete('/api/persons/:id', (request, response, next) => {
   .catch(error => next(error))
 })
 
-app.get('/info', (request, response) => {
-  const line1 = `<div>Phonebook has info for ${persons.length} people</div>`
-  const line2 = new Date().toString()
-  response.send(line1 + '<br>' + line2)
+app.get('/info', (request, response, next) => {
+  Contact.countDocuments()
+  .then(result => {
+    const line1 = `<div>Phonebook has info for ${result} people</div>`
+    const line2 = new Date().toString()
+    response.send(line1 + '<br>' + line2)
+  })
+  .catch(error => next(error))
 })
 
 app.use(errorHandler)
