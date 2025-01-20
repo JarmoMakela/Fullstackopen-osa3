d04c87000971f5479d8d1a44a621b554ee3df56e
Jarmo <jarmo.makela@tietoevry.com>
2025-01-20 14:29:42 +0200

    tehtävät 3.19, 3.20 ja 3.21

--- index.js
+++ index.js
@@ -27,6 +27,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
   }
 
   next(error)
@@ -63,24 +65,30 @@ app.post('/api/persons', (request, response, next) => {
     number: body.number
   })
 
-  contact.save().then(savedContact => {
+  contact.save()
+  .then(savedContact => {
     response.json(savedContact)
   })
   .catch(error => next(error))
 })
 
 app.put('/api/persons/:id', (request, response, next) => {
-  const body = request.body
+  const { name, number } = request.body
+  /*const body = request.body
   const contact = {
     name: body.name,
     number: body.number
-  }
+  }*/
 
-  if (!body.name || !body.number) {
+  if (!number) {
     return response.status(400).json({error: 'content missing'})
   }
 
-  Contact.findByIdAndUpdate(request.params.id, contact, { new: true })
+  Contact.findByIdAndUpdate(
+    request.params.id,
+    [{ name, number }], 
+    { new: true, runValidators: true, context: 'query' }
+  )
     .then(updatedContact => {
       response.json(updatedContact)
     })
--- models/contact.js
+++ models/contact.js
@@ -14,8 +14,33 @@ mongoose.connect(url)
   })
 
 const contactSchema = new mongoose.Schema({
-  name: String,
-  number: String
+  name: {
+    type: String,
+    minlength: 3,
+    required: true
+  },
+  number: { 
+    type: String,
+    minlength: 8,
+    validate : {
+      validator: function(str) {
+        const arr = str.split('-')
+        const regex = /^\d+$/;
+        if (arr.length === 2 // A hyphen separates the string into two parts
+            && regex.test(arr[0]) // The first part contains only numbers
+            && regex.test(arr[1]) // The second part contains only numbers
+            && arr[0].length >= 2 && arr[0].length <= 3 // The first part is 2 or 3 numbers
+            && (arr[0].length + arr[1].length >= 8) // Total amount of numbers
+            ) {
+          return true
+        } else {
+          return false
+        }
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    },
+    required: [true, 'User phone number required']
+  }
 })
 
 contactSchema.set('toJSON', {
